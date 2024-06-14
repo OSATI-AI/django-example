399c88ff79ce396ef119ad6a52341747dd937015
Alexander Braun <abraun@rml-legion.mpi.localnet>
2024-06-14 13:26:34 +0200

    Structure tasks to topics, key ideas and classes and add structure to the sidebar

--- app/views.py
+++ app/views.py
@@ -5,18 +5,61 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from jinja2 import Template
 
-LANGAUGE = "english"
+LANGUAGE = "english"
+from collections import defaultdict
 
-def index(request):
-    tasks_dir = os.path.join(os.path.dirname(__file__), 'data/math')
+def load_tasks(tasks_dir):
     tasks = []
+    # Load all tasks in the given directory and create list of tuples (title, filename, topic_id)
     for task_file in os.listdir(tasks_dir):
         if task_file.endswith('.yaml'):
             with open(os.path.join(tasks_dir, task_file), 'r') as file:
                 task = yaml.safe_load(file)
-                tasks.append((task['title'][LANGAUGE], task_file))
+                tasks.append((task['title'][LANGUAGE], task_file, task['topic_id']))
+    return tasks
 
-    return render(request, 'app/index.html', {'tasks': tasks})
+def structure_tasks(tasks, topics_lookup):
+    
+    structured_tasks = {}
+
+    for title, filename, topic_id in tasks:
+        topic = topics_lookup['topics'][topic_id]
+        key_idea_id = topic['key_idea']
+        level_id = topic['level']
+
+        level_name = topics_lookup['levels'][level_id][LANGUAGE]
+        key_idea_name = topics_lookup['key_ideas'][key_idea_id][LANGUAGE]
+        topic_name = topic['title'][LANGUAGE]
+
+        if level_name not in structured_tasks:
+            structured_tasks[level_name]={}
+
+        if key_idea_name not in structured_tasks[level_name]:
+            structured_tasks[level_name][key_idea_name] = {}
+        
+        if topic_name not in structured_tasks[level_name][key_idea_name]:
+            structured_tasks[level_name][key_idea_name][topic_name] = []
+        
+        structured_tasks[level_name][key_idea_name][topic_name].append((title, filename))
+
+    return structured_tasks
+
+def index(request):
+    # Load all tasks in the given directory
+    tasks_dir = os.path.join(os.path.dirname(__file__), 'data/math')
+    tasks = load_tasks(tasks_dir)
+
+    # Load topics lookup
+    with open(os.path.join(os.path.dirname(__file__), 'data/topics_lookup.yaml'), 'r') as file:
+        topics_lookup = yaml.safe_load(file)
+
+    # Structure tasks
+    structured_tasks = structure_tasks(tasks, topics_lookup)
+
+    print(structured_tasks)
+
+    # Render index.html and display the tasks in the sidebar
+    return render(request, 'app/index.html', {'structured_tasks': structured_tasks})
 
 def fill_text(yaml_file, language):
     # Convert YAML data to a string
@@ -47,7 +90,7 @@ def load_task(request):
 
     task = open(task_file, "r").read()
     task = yaml.safe_load(task)
-    task = fill_text(task, language=LANGAUGE)
+    task = fill_text(task, language=LANGUAGE)
     template_name = task["template"]+".yaml"
     template = open(os.path.join(template_dir, template_name), "r").read()
     template = yaml.safe_load(template)
